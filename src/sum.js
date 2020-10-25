46d5787d229ba7402c8c0bae0e7feb3dacc30669
rixong <rixong@gmail.com>
2020-10-25 08:42:44 -0500

    added a few initial tests

---
+++ src/sum.js
@@ -0,0 +1,13 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function makeDog(name, breed) {
+  return { name, breed };
+}
+
+module.exports = { sum, multiply, makeDog };
